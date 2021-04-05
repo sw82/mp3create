afa650b5d7553b2f0950f06de827d0ce471126c8
bass.t <fromsucktoblow@gmail.com>
2021-04-05 23:28:53 +0200

    added script

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+rm files.txt
+
+for i in *.mp3; do
+    # Will print */ if no directories are available
+   echo "file '$i'" >> files.txt
+done
+
+ffmpeg -f concat -safe 0 -i files.txt -c copy output.mp3
